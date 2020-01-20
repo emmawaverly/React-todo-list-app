b4db31e843eaee451020cf82bcf152c9efc436c9
Waner Deng <dengwaner@gmail.com>
2020-01-19 21:48:01 -0800

    Updates

---
+++ precache-manifest.8fc8ec3bfc952cb8b97dba41352fadd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e1ce472c0b76fb090d62cd94fbeebf",
+    "url": "/emmawaverly/React-todo-list-app.git/index.html"
+  },
+  {
+    "revision": "364bfc08361dc3fe65db",
+    "url": "/emmawaverly/React-todo-list-app.git/static/css/main.29fa90b6.chunk.css"
+  },
+  {
+    "revision": "bbb5a38eefcbe83f12ad",
+    "url": "/emmawaverly/React-todo-list-app.git/static/js/2.44b538b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/emmawaverly/React-todo-list-app.git/static/js/2.44b538b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "364bfc08361dc3fe65db",
+    "url": "/emmawaverly/React-todo-list-app.git/static/js/main.1437157d.chunk.js"
+  },
+  {
+    "revision": "c4a1ee0f7b7850b2e8a3",
+    "url": "/emmawaverly/React-todo-list-app.git/static/js/runtime-main.b877fcb1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/emmawaverly/React-todo-list-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
